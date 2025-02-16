a22eb04f78b5f1657e0d7ca0bec7824e7aee9747
Ritul Dani <100018102+rituldani@users.noreply.github.com>
2025-02-16 18:00:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    {/* <Footer/> */}
+    </>
+  );
+}
+
+export default App;
